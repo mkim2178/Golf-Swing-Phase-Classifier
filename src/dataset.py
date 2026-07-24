28d532c680d8118cb28d03aa51f165a9d01d413c
mkim2178 <mkim2178@berkeley.edu>
2026-07-24 16:26:12 +0900

    structure of the Dataset object has been changed

--- src/dataset.py
+++ src/dataset.py
@@ -1,33 +1,15 @@
-from pathlib import Path
 from PIL import Image
-
 from torch.utils.data import Dataset
 
 class GolfSwingDataset(Dataset):
-    def __init__(self, data_dir, transform=None):
+    def __init__(self, samples, transform=None):
         """
-        data_dir: a Path object of a root directory
-        transform: a transformation pipeline that will be applied into images
-        class_to_idx: a dictionary that maps the class name into an integer label
-        samples: a list of tuples (image path, class index)
+        samples: a list of tuples (Path object, swing-phase label)
+        transform: a transformation pipeline that will be applied to images
         """
 
-        self.data_dir = Path(data_dir)
+        self.samples = samples
         self.transform = transform
-        self.class_to_idx = {
-            "address": 0,
-            "top": 1,
-            "impact": 2,
-            "finish": 3
-        }
-        self.samples = []
-        for cls, idx in self.class_to_idx.items():
-            # create a path to the current class directory
-            class_dir = self.data_dir / cls
-
-            # append every image path and index by tuple
-            for img_path in sorted(class_dir.glob("*.PNG")):
-                self.samples.append((img_path, idx))
     
     def __len__(self):
         """
