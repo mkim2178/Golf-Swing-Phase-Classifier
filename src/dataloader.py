8064aab11de5e66048d52325af16914e7c3ff31c
mkim2178 <mkim2178@berkeley.edu>
2026-07-24 16:25:02 +0900

    logic of splitting dataset has been changed and actual datasets has been initialized

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,38 +1,38 @@
 import random
+from pathlib import Path
 from torchvision import transforms
 from dataset import GolfSwingDataset
+import matplotlib.pyplot as plt
+
+"""
+For each setting (10 golf swings), split into a ratio of 6:2:2 (train:validation:test)
+- train: 18 golf swings * 4 phases = 72 images
+- validation: 6 golf swings * 4 phases = 24 images
+- test: 6 golf swings * 4 phases = 24 images
+total: 30 golf swings * 4 phases = 120 images
+"""
 
 # for reproducibility
 random.seed(42)
 
+# labels
+setting_labels = ["set0" + str(i) for i in range(1, 4)]
+
+phase_labels = [("address", 0), ("top", 1), ("impact", 2), ("finish", 3)]
+
 # lists that will contain each train, validation, test swing phases
-train = []
-val = []
-test = []
+train_samples = []
+val_samples = []
+test_samples = []
 
+def split_swings():
 
-def train_val_test_split():
-    """
-    a function that randomly shuffles and splits swing labels into a ratio of 6:2:2 (train:val:test) for each swing setting
-    """
-
-    # 10 swings for each setting
-    swing_labels = ["swing01_",
-                    "swing02_",
-                    "swing03_",
-                    "swing04_",
-                    "swing05_",
-                    "swing06_",
-                    "swing07_",
-                    "swing08_",
-                    "swing09_",
-                    "swing10_",
-                    ]
+    swing_labels = ["swing0" + str(i) for i in range(1, 10)] + ["swing10"]
     
     # randomly shuffle the swing_labels list
     random.shuffle(swing_labels)
 
-    # list slicing for each train, validation, and test swings
+    # split the swing_labels into a ratio of 6:2:2 (train:val:test) for each setting of golf swing
     train_swings = swing_labels[:6]
     val_swings = swing_labels[6:8]
     test_swings = swing_labels[8:]
@@ -41,29 +41,30 @@ def train_val_test_split():
 
 
 # for each setting, call the spliting function
-for setting in ["set01_", "set02_", "set03_"]:
-    train_swings, val_swings, test_swings = train_val_test_split()
+for setting in setting_labels:
+    train_swings, val_swings, test_swings = split_swings()
 
     # for each train, val, and test swings, append each swing's phase image
-    for phase in ["address", "top", "impact", "finish"]:
-        train += [setting + swing + phase + ".PNG" for swing in train_swings]
-        val += [setting + swing + phase + ".PNG" for swing in val_swings]
-        test += [setting + swing + phase + ".PNG"for swing in test_swings]
+    for phase in phase_labels:
 
-"""
-for each setting (10 golf swings), split into a ratio of 6:2:2 (train:validation:test)
-- train: 18 golf swings * 4 phases = 72 images
-- validation: 6 golf swings * 4 phases = 24 images
-- test: 6 golf swings * 4 phases = 24 images
-total: 30 golf swings * 4 phases = 120 images
-"""
-for t in train:
-    print(t)
-print(f"Number of images for train: {len(train)}")
-print(f"Number of images for validation: {len(val)}")
-print(f"Number of images for test: {len(test)}")
+        phase_name, phase_label = phase
 
+        phase_dir = Path("data") / phase_name
 
+        for swing in train_swings:
+            file_name = setting + "_" + swing + "_" + phase_name + ".PNG"
+            image_path = phase_dir / file_name
+            train_samples.append((image_path, phase_label))
+        
+        for swing in val_swings:
+            file_name = setting + "_" + swing + "_" + phase_name + ".PNG"
+            image_path = phase_dir / file_name
+            val_samples.append((image_path, phase_label))
+
+        for swing in test_swings:
+            file_name = setting + "_" + swing + "_" + phase_name + ".PNG"
+            image_path = phase_dir / file_name
+            test_samples.append((image_path, phase_label))
 
 
 # define the pipeline of the image transformation
@@ -72,4 +73,10 @@ transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-# WORK IN PROGRESS (NEED TO DEFINE THE DATALOADER AND THE ACTUAL DATASET THAT WILL BE USED IN THE MODEL)
+
+train_dataset = GolfSwingDataset(train_samples, transform)
+val_dataset = GolfSwingDataset(val_samples, transform)
+test_dataset = GolfSwingDataset(test_samples, transform)
+
+for sample in train_dataset.samples:
+    print(sample)
