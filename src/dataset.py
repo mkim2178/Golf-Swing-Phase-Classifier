f4f8a33609849f49652316c05626e2364600109f
mkim2178 <mkim2178@berkeley.edu>
2026-07-28 11:58:46 +0900

    edited code explanations

--- src/dataset.py
+++ src/dataset.py
@@ -2,15 +2,15 @@ from PIL import Image
 from torch.utils.data import Dataset
 
 class GolfSwingDataset(Dataset):
+
     def __init__(self, samples, transform=None):
         """
-        samples: a list of tuples (Path object, swing-phase label)
+        samples: a list of tuples (Path object, numeric label for each swing-phase)
         transform: a transformation pipeline that will be applied to images
         """
-
         self.samples = samples
         self.transform = transform
-    
+
     def __len__(self):
         """
         return the length of "samples" list
@@ -19,8 +19,8 @@ class GolfSwingDataset(Dataset):
     
     def __getitem__(self, idx):
         """
-        get a certain image's path and label from samples list, convert into RGB image, and apply transformation if it's assigned
-        return the image and the numeric label
+        retrieve idx-th image's path and label from the `samples` list, convert it into RGB image, and apply the image transformation if it's assigned
+        return the image and the numeric label of the swing-phase
         """
         img_path, label = self.samples[idx]
         with Image.open(img_path) as img:
