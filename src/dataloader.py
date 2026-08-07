ed5ad113fb311725b05fc5238918aebfa699d333
mkim2178 <mkim2178@berkeley.edu>
2026-08-07 15:35:39 +0900

    organized comments and codes

--- src/dataloader.py
+++ src/dataloader.py
@@ -4,17 +4,17 @@ from torchvision import transforms
 from dataset import GolfSwingDataset
 from torch.utils.data import DataLoader
 
-# reproducibility purpose
+# controls reproducibility of the swing-level train/validation/test split
 RANDOM_SEED = 42
 
 
-# setting labels will be used to retrieve image files
-SETTING_LABELS = ["set0" + str(i) for i in range(1, 4)]
+# setting labels used to retrieve image files
+SETTING_LABELS = [f"set{i:02d}" for i in range(1, 4)]
 
-# labels for each swing-phase (can be replaced with using a dictionary)
+# swing-phase names and their corresponding numeric labels
 PHASE_LABELS = [("address", 0), ("top", 1), ("impact", 2), ("finish", 3)]
 
-# a pipeline for the image transformation (resize and convert the image into a Tensor object)
+# an image transformation pipeline for resizing and converting into a Tensor
 TRANSFORM = transforms.Compose([
     transforms.Resize((256, 128)),
     transforms.ToTensor()
@@ -23,15 +23,13 @@ TRANSFORM = transforms.Compose([
 
 def split_swings():
     """
-    shuffle the order of swing labels and split them into a ratio of 6:2:2 (train:val:test) for each setting of the golf swing
+    shuffle the swing labels and split them into training, validation and test groups using a 6:2:2 ratio
     """
-
-    # "swing10" starts with "swing1" so it's has been separately added
-    swing_labels = ["swing0" + str(i) for i in range(1, 10)] + ["swing10"]
     
+    swing_labels = [f"swing{i:02d}" for i in range(1, 11)]
     random.shuffle(swing_labels)
 
-    # list slice the shuffled swing_labels to assign train, val, and test list
+    # split the shuffled labels into training, validation, and test groups
     train_swings = swing_labels[:6]
     val_swings = swing_labels[6:8]
     test_swings = swing_labels[8:]
@@ -41,51 +39,47 @@ def split_swings():
 
 def create_datasets():
     """
-    create datasets for train, val, and test
+    create the training, validation, and test datasets
     """
     random.seed(RANDOM_SEED)
 
-    # init lists that will contain a tuple (image path, numeric label) for each train, val, and test
+    # store (image path, numeric label) pairs for each dataset
     train_samples = []
     val_samples = []
     test_samples = []
 
-    # for each setting labels (set0, set1, set2)
+    # create a separate swing-level split for each setting (set01, set02, set03)
     for setting in SETTING_LABELS:
 
         train_swings, val_swings, test_swings = split_swings()
 
-        # for each train, val, and test swings, append each swing's phase image
-        for phase in PHASE_LABELS:
+        # add each phase image to the spilt assigned to its swing
+        for phase_name, phase_label in PHASE_LABELS:
 
-            # each phase has the name and the assigned numeric label (ex: (address, 0))
-            phase_name, phase_label = phase
-
-            # init a Path object with using the root directory and the phase name 
+            # create the directory path for the current swing phase
             phase_dir = Path("data") / phase_name
 
             # iterate six swings for training dataset
+            # create the image path and append its path-label pair
             for swing in train_swings:
-
-                # init the name of image file, extend the Path object with the current image, and append it into the train_samples list as a tuple (image path, numeric swing-phase label)
-                file_name = setting + "_" + swing + "_" + phase_name + ".PNG"
+                file_name = f"{setting}_{swing}_{phase_name}.PNG"
                 image_path = phase_dir / file_name
                 train_samples.append((image_path, phase_label))
 
             # same logic as the training dataset
             for swing in val_swings:
-                file_name = setting + "_" + swing + "_" + phase_name + ".PNG"
+                file_name = f"{setting}_{swing}_{phase_name}.PNG"
                 image_path = phase_dir / file_name
                 val_samples.append((image_path, phase_label))
 
             # same logic as the training dataset
             for swing in test_swings:
-                file_name = setting + "_" + swing + "_" + phase_name + ".PNG"
+                file_name = f"{setting}_{swing}_{phase_name}.PNG"
                 image_path = phase_dir / file_name
                 test_samples.append((image_path, phase_label))
 
 
-    # init train, validation, and test datasets with applying each samples list and the transformation pipeline that has been initialized above
+    # create the datasets using their sample lists and the shared transform
     train_dataset = GolfSwingDataset(train_samples, TRANSFORM)
     val_dataset = GolfSwingDataset(val_samples, TRANSFORM)
     test_dataset = GolfSwingDataset(test_samples, TRANSFORM)
@@ -95,17 +89,16 @@ def create_datasets():
 
 def create_dataloaders(batch_size=8):
     """
-    create dataloaders for train, val, and test
+    create DataLoaders for the training, validation, and test datasets
     """
 
-    # init datasets
     train_dataset, val_dataset, test_dataset = create_datasets()
 
     # init train, validation, and test dataloaders that will be used into the actual model
-    # batch size is 8 for all dataloaders
+    # use the provided batch size for all DataLoaders
     # shuffle only the training data so each epoch receives batches in a different order
-    train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size, shuffle=False)
-    test_dataloader = DataLoader(test_dataset, batch_size, shuffle=False)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     return train_dataloader, val_dataloader, test_dataloader
