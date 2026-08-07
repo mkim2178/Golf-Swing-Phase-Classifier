7be409ada53e7c64ed54907f1e68191ae3af088e
mkim2178 <mkim2178@berkeley.edu>
2026-08-07 15:36:19 +0900

    comments has been updated

--- src/model.py
+++ src/model.py
@@ -2,10 +2,10 @@ from torch import nn
 
 class ConvBlock(nn.Module):
     """
-    a convolutional block with including:
-    - Conv2d (a 2 dimensional convolutional layer with a kernel size of 3, stride size 1, and 1 padding)
-    - ReLU (a relu activation function with inplacement)
-    - MaxPool2d (a 2 dimensional max pooling layer with a kernel size of 2 and stride of 2)
+    a convolutional block containing:
+    - Conv2d (a 2D convolutional layer with a kernel size of 3, stride size 1, and 1 padding)
+    - ReLU (a relu activation function with in-place operation)
+    - MaxPool2d (a 2D max pooling layer with a kernel size of 2 and stride of 2)
     """
     def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
         super(ConvBlock, self).__init__()
@@ -22,14 +22,24 @@ class ConvBlock(nn.Module):
 
 class CNN(nn.Module):
     """
-    a Convolutional Neural Network of the model
-    since we have RGB images, the first input channel would be 3 and we have four different swing-phases, the output number of class would be 4
-    - block1: a ConvBlock with input channels = 3, output channels = 16, kernel size of 3, stride size 1, and 1 padding
-    - block2: a ConvBlock with input channels = 16, output channels = 32, kernel size of 3, stride size 1, and 1 padding
-    - block3: a ConvBlock with input channels = 32, output channels = 64, kernel size of 3, stride size 1, and 1 padding
-    - adaptive_pool: an Adaptive Average Pooling with using size of 4 (this manages a linear layer having a vast amount of features and occur overfitting)
-    - flatten: a Flatten layer that converts current shape from [batch size, channel, height, width] to [batch size, channel * height * width]
-    - linear: a Linear layer that returns each score of the swing phase
+    Explanation:
+    - a Convolutional Neural Network for classifying four golf-swing phases
+    - since we have RGB images, the first input channel would be 3 and we have four different swing-phases, the number of output classes is 4
+        * block1: a ConvBlock with input channels = 3, output channels = 16, kernel size of 3, stride size 1, and 1 padding
+        * block2: a ConvBlock with input channels = 16, output channels = 32, kernel size of 3, stride size 1, and 1 padding
+        * block3: a ConvBlock with input channels = 32, output channels = 64, kernel size of 3, stride size 1, and 1 padding
+        * adaptive_pool: reduces each feature map to 4 x 4, limiting the number of inputs and parameters in the final linear layer
+        * flatten: a Flatten layer that converts current shape from [batch size, channel, height, width] to [batch size, channel * height * width]
+        * linear: a Linear layer that returns each score of the swing phase
+
+    Architecture:
+    * input: [batch size, 3, 256, 128]
+    * block1: [3, 256, 128] -> [16, 128, 64]
+    * block2: [16, 128, 64] -> [32, 64, 32]
+    * block3: [32, 64, 32] -> [64, 32, 16]
+    * adaptive_pool: [64, 32, 16] -> [64, 4, 4]
+    * flatten: 64 * 4 * 4 = 1024 features
+    * linear: 1024 features -> 4 class scores
     """
     def __init__(self, in_channels=3, num_classes=4):
         super(CNN, self).__init__()
